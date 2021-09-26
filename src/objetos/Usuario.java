345b7b390e4ee13061a129918df316c9a1c8b0b9
AngelLopezM <76121102+AngelLopezM@users.noreply.github.com>
2021-09-26 10:33:35 -0500

    Creada el objeto Usuario
    Parametros:
    - usuarioID
    - nombre
    - apellido
    - correo
    - genero
    - fechaNacimiento
    - contraseña

---
+++ src/objetos/Usuario.java
@@ -0,0 +1,39 @@
+package objetos;
+
+public class Usuario {
+
+    /**
+     * ID del usuario
+     */
+    private int usuarioID;
+
+    /**
+     * Nombre del usuario
+     */
+    private String nombre;
+
+    /**
+     * Apellido del usuario
+     */
+    private String apellido;
+
+    /**
+     * Correo del usuario
+     */
+    private String correo;
+
+    /**
+     * Contraseña del usuario
+     */
+    private String contrasena;
+
+    /**
+     * Genero del usuario
+     */
+    private String genero;
+
+    /**
+     * Fecha de nacimiento del usuario
+     */
+    private String fechaNacimiento;
+}
